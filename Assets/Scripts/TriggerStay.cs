6a5176c7a5c65538299b3f1b0ef182648854f300
UFAD\mohanzalake <mohanzalake123@gmail.com>
2017-08-04 16:31:36 -0400

    Added Juliet scene

---
+++ Assets/Scripts/TriggerStay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TriggerStay: MonoBehaviour {
+	
+	void Start () {	
+	}
+	
+	void Update () {
+	}
+
+    private void OnTriggerStay (Collider player)
+    {
+        Debug.Log("player is within the trigger");
+    }   
+}

7967fe17d57b1de3f4d0e302bbad1475e8a1907a
Samantha Garcia <sgarcia22@ufl.edu>
2017-08-08 11:37:14 -0400

    Added Simple Main Menu Scene
    -Added OVR objects to Main Menu
    -Simple Panel & Title Displayed
    -Button Highlights in Yellow when Gaze looks at it
    -Changes Scene when User Looks at Button and Presses Spacebar

---
+++ Assets/Scripts/Main_Menu/VRButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class VRButton : MonoBehaviour {
+
+	private Button changeScene;
+
+	//Get current button attached
+	void Start () {
+		changeScene = gameObject.GetComponent<Button> ();
+	}
+
+	//When user looks at button
+	public void OnGazeEnter() {
+		Debug.Log ("Looking at Button");
+	}
+
+	//Change Scene to Hurricane One
+	public void loadHurricaneScene() {
+		SceneManager.LoadScene (1);
+	}
+}
